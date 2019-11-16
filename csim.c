6fd3d00b09c7f735bc95ce5317d256e77d00b798
studio-luke <goalone4r@postech.ac.kr>
2019-11-16 17:22:10 +0900

    21 score

--- csim.c
+++ csim.c
@@ -15,6 +15,12 @@ typedef struct {
 	int B_siz;
 } cache_conf;
 
+typedef struct {
+	int valid;
+	long long int tag;
+	long timestamp;
+} cache_line;
+
 void print_help(char *argv[]) { 
 	printf("Usage: %s [-hv] -s <num> -E <num> -b <num> -t <file>\n", argv[0]);
     printf("Options:\n");
@@ -25,7 +31,6 @@ void print_help(char *argv[]) {
     printf("  -E <num>   Number of lines per set.\n");
     printf("  -b <num>   Number of block offset bits.\n");
     printf("  -t <file>  Trace file.\n");
-    exit(0);
 }
 
 
@@ -36,9 +41,15 @@ int main(int argc, char *argv[])
 	FILE *trace;			// stores trace file stream
 	int is_verbose = 0;		// verbose mode
 
-	int **cache;
-	char instr[20];
-	int i;
+	cache_line **cache;
+	char instr[20], buf[20];
+
+	long long int addr, tag;
+	int block, set;
+
+	int hit = 0, miss = 0, eviction = 0;
+	int is_hit, is_eviction, is_Mhit;
+	int i, cnt = 0, min, min_idx;
 
 	/* Temporary variables for user input */
 	char trace_name[32];	// filename of trace input
@@ -73,26 +84,83 @@ int main(int argc, char *argv[])
 	
 	cache_prop.S_siz = 1 << cache_prop.s;
 	cache_prop.B_siz = 1 << cache_prop.b;
-	cache = (int**)calloc(cache_prop.S_siz, sizeof(int*));
+	cache = (cache_line**)calloc(cache_prop.S_siz, sizeof(cache_line*));
 	for (i = 0; i < cache_prop.S_siz; i++)
-		cache[i] = (int*)calloc(cache_prop.B_siz, sizeof(int));
-	printf("%d\n", cache[0][0]);
+		cache[i] = (cache_line*)calloc(cache_prop.E, sizeof(cache_line));
 
 	while (fgets(instr, sizeof(instr), trace) != NULL) {
+		cnt++;
 		if (instr[0] == 'I') continue;
+		is_hit = is_Mhit = is_eviction = 0;
+		instr[strcspn(instr, "\n")] = '\0';
 		
-		switch (*strtok(instr, " ,")) {
+		strcpy(buf, instr);
+		switch (*strtok(buf, " ,")) {
+		case 'M':
+			is_Mhit = 1;
+			hit += 1;
 		case 'L':
 		case 'S':
-		case 'M':
-			
-		defalut: printf("ERROR!!"); exit(-1);
-		}
+			addr = strtol(strtok(NULL, " ,"), NULL, 16);
+			block = addr % cache_prop.B_siz;
+			addr /= cache_prop.B_siz;
 
+			set = addr % cache_prop.S_siz;
+			addr /= cache_prop.S_siz;
+
+			tag = addr;
+			// printf("b: %d, s: %d, t: %lld\n", block, set, tag);
+			if (block) {}
+
+			/* To be 'HIT', Search for the given data */
+			for (i = 0; i < cache_prop.E; i++)
+				if (cache[set][i].valid == 1 && cache[set][i].tag == tag) {
+					cache[set][i].timestamp = cnt;
+					
+					hit += 1;
+					is_hit = 1;
+					break;
+				}
+			if (is_hit) break;
+			miss += 1;
+
+			is_eviction = 1;
+			/* To be just 'MISS', Search for available space*/
+			for (i = 0; i < cache_prop.E; i++)
+				if (cache[set][i].valid == 0) {
+					cache[set][i].valid = 1;
+					cache[set][i].tag = tag;
+					cache[set][i].timestamp = cnt;
+					
+					is_eviction = 0;
+					break;
+				}
+			if (!is_eviction) break;
+			
+			/* To be 'EVICTION', LRU Policy*/
+			eviction += 1;
+			min_idx = 0;
+			min = cache[set][min_idx].timestamp;
+			for (i = 0; i < cache_prop.E; i++) {
+				if (min > cache[set][min_idx].timestamp) {
+					min = cache[set][min_idx].timestamp;
+					min_idx = i;
+				}
+			}
+
+			cache[set][min_idx].valid = 1;
+			cache[set][min_idx].tag = tag;
+			cache[set][min_idx].timestamp = cnt;
+			break;
+		default: printf("ERROR!!\n"); exit(-1);
+		}
+		if (is_verbose)
+			printf("%s %s%s%s\n", instr + 1, is_hit ? "hit " : "miss ", 
+										is_eviction ? "eviction " : "",
+										is_Mhit? "hit " : "");
 	}
 
-	if (!is_verbose)
-		printSummary(0, 0, 0);
+	printSummary(hit, miss, eviction);
 	
 	for (i = 0; i < cache_prop.S_siz; i++)
 		free(cache[i]);
